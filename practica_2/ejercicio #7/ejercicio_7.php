cd79f1b693510521fa74239e61d994afc5963fc1
Grower19 <growerquenallata665@gmail.com>
2019-08-25 17:01:11 -0400

    practica 2
    se realizo la mayoria de los ejercicios

---
+++ practica_2/ejercicio #7/ejercicio_7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ejercicio_7</title>
+</head>
+<body>
+    <h1>practica 7</h1>
+    <h2>escribe un programa que calcule el total de una factura a partir de la base imponible</h2>
+    <p>
+    
+    </p>
+    
+</body>
+</html>
---
+++ practica_2/ejercicio #8/ejercicio_8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ejercicio_8</title>
+</head>
+<body>
+    <h1>practica 8</h1>
+    <h2>escribe un programa que calcule el salario semanal de un trabajador en base a las horas trabajadas se pagaran 12 euros por hora </h2>
+<p>
+
+</p>
+    
+</body>
+</html>
